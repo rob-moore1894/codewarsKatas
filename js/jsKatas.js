883b8bf970866ce8df893d5333352bef06264f85
rob-moore1894 <moore.re@gmail.com>
2020-02-13 12:56:12 -0600

    Updated HTML and added CSS for your viewing pleasure

---
+++ js/jsKatas.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// Your task is to create a function that does four basic mathematical operations.
+//
+//     The function should take three arguments - operation(string/char), value1(number), value2(number).
+//     The function should return result of numbers after applying the chosen operation.
+
+
+// Basic Mathematical Operations
+function basicOp(operation, value1, value2) {
+    var result;
+    switch (operation) {
+        case '*':
+            result = value1 * value2;
+            break;
+        case '/':
+            result = value1 / value2;
+            break;
+        case '+':
+            result = value1 + value2;
+            break;
+        case '-':
+            result = value1 - value2;
+            break;
+    }
+
+    return result;
+
+}
+
+console.log(basicOp('*',10,5));
+// 10 * 5 = 50
+
+//Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
