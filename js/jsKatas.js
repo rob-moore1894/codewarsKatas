0b11dbc14bfc30312db9e4f260cd150b87f25577
rob-moore1894 <moore.re@gmail.com>
2020-02-13 12:57:00 -0600

    Updated HTML and added CSS

--- js/jsKatas.js
+++ js/jsKatas.js
@@ -1,5 +1,8 @@
 "use strict";
 
+$(document).ready(function () {
+    console.log("Ready to roll!");
+});
 // Your task is to create a function that does four basic mathematical operations.
 //
 //     The function should take three arguments - operation(string/char), value1(number), value2(number).
@@ -28,8 +31,7 @@ function basicOp(operation, value1, value2) {
 
 }
 
-console.log(basicOp('*',10,5));
-// 10 * 5 = 50
+
 
 //Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
 
