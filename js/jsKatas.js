01fb1ec112f022798180fb9f6f48bbe3c797d474
rob-moore1894 <moore.re@gmail.com>
2020-02-14 17:07:43 -0600

    Added more practice Katas

--- js/jsKatas.js
+++ js/jsKatas.js
@@ -31,7 +31,44 @@ function basicOp(operation, value1, value2) {
 
 }
 
-
+console.log(basicOp('*',10,5));
 
 //Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+function summation(num) {
+    var total = 0;
+    for (var i = 1; i <= num; i++){
+        total += i;
+    }
+    return total;
+}
+console.log(summation(3));
+
+//Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+function findNextSquare(sq) {
+    // Return the next square if sq if a perfect square, -1 otherwise
+    // Find the square root of the number
+    // If the square root is an integer, add 1 to the root and square it, then return that number
+    var squareRoot = Math.sqrt(sq);
+
+    if (squareRoot % 1 === 0){
+        var addOne = squareRoot + 1;
+        var nextSquare = addOne * addOne;
+        return nextSquare;
+    } else {
+        return -1;
+    }
+}
+$('.kataQuestions').html(findNextSquare(121));
+
+console.log(findNextSquare(121));
+
+//Write a function called repeatString which repeats the given String src exactly count times.
+function repeatStr (n, s) {
+    if (!isNaN(n) && n > 0) {
+        return s.repeat(n)
+    }
+}
+
+console.log(repeatStr(3, "Yo!"));
 
