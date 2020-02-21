c8e62cd0fac51b6e2deb572e15115b24c589d006
rob-moore1894 <moore.re@gmail.com>
2020-02-20 19:36:44 -0600

    Added personal code.  Working on interfacing with the DOM for usability

--- js/jsKatas.js
+++ js/jsKatas.js
@@ -45,23 +45,27 @@ console.log(summation(3));
 
 //Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
 
-function findNextSquare(sq) {
-    // Return the next square if sq if a perfect square, -1 otherwise
-    // Find the square root of the number
-    // If the square root is an integer, add 1 to the root and square it, then return that number
-    var squareRoot = Math.sqrt(sq);
+//$('#calculateSquare').onclick(console.log($('#inputSquare').value));
 
-    if (squareRoot % 1 === 0){
-        var addOne = squareRoot + 1;
-        var nextSquare = addOne * addOne;
-        return nextSquare;
-    } else {
-        return -1;
-    }
-}
-$('.kataQuestions').html(findNextSquare(121));
 
-console.log(findNextSquare(121));
+// function findNextSquare() {
+//
+//     // Return the next square if sq if a perfect square, -1 otherwise
+//     // Find the square root of the number
+//     // If the square root is an integer, add 1 to the root and square it, then return that number
+//     console.log($('#inputSquare').value);
+//     var sq = parseInt($('#inputSquare').value);
+//     console.log(sq);
+//     var squareRoot = Math.sqrt(sq);
+//
+//     if (squareRoot % 1 === 0){
+//         var addOne = squareRoot + 1;
+//         var nextSquare = addOne * addOne;
+//         $('.kataAnswers').html = nextSquare;
+//         return nextSquare;
+//     }
+//
+// }
 
 //Write a function called repeatString which repeats the given String src exactly count times.
 function repeatStr (n, s) {
@@ -71,4 +75,20 @@ function repeatStr (n, s) {
 }
 
 console.log(repeatStr(3, "Yo!"));
+
+// This has nothing to do with Codewars
+// I just wanted to calculate about how much I would make on a 12 Month Contract in Afghanistan
+// According to the email, first 6 months is $185.30 per working day
+// After the first 6 months, the pay is $194.89 per working day
+// For consideration - there is a 30 day vacation (rotation) taken during the second half of the year
+
+function salary(firstHalf, secondHalf, workingDaysPerWeek){
+    // firstHalf is the pay for the first 6 months
+     var firstPay = (((firstHalf * workingDaysPerWeek) * 4) * 6);
+     var secondPay = (((secondHalf * workingDaysPerWeek) * 4) * 5);
+     var totalPay = firstPay + secondPay;
+     return "For the year: $" + totalPay.toFixed(2);
+}
+
+console.log(salary(185.30, 194.89, 6));
 
